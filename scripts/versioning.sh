952545747bdbc099c5d8a2126c8fdbaffa259c5f
Javier-Mobysoft <javier.gomez@mobysoft.com>
2017-01-11 12:42:41 +0000

    Make it executable

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -2,4 +2,4 @@
 commit_date=`git show -s --format="%ct"`
 formatted_date=`date -d @${commit_date} +"%Y%m%d%H%M"`
 incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
-mvn versions:set -DnewVersion=${formatted_date}-${incremental_version}-SNAPSHOT
+mvn versions:set -DnewVersion=${formatted_date}-${incremental_version}-SNAPSHOT  
